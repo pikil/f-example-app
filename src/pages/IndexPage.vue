aa7fc04ed23be9f86ef4c106e567df84a9c1a367
Ilia Pikulev <cobanja1@gmail.com>
2022-04-13 21:44:18 +1200

    Adding index page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,32 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <q-card bordered flat>
+      <q-card-section>
+        <p class="text-center text-subtitle1 q-mb-none">This is an example of the app for tradies.</p>
+      </q-card-section>
+      <q-card-section class="text-center">
+        <q-btn
+          rounded
+          color="primary"
+          label="View jobs"
+          to="/admin/jobs"
+          size="lg"
+          class="q-px-lg"
+          outline
+        />
+      </q-card-section>
+      <q-card-section>
+        <p>Featuring tech stack:</p>
+        <ul>
+          <li>VueJS;</li>
+          <li>Vue Router (page routing within SPA);</li>
+          <li>Pinia (State management);</li>
+          <li>Quasar Framework;</li>
+          <li>Dexie (wrapper for IndexedDB);</li>
+          <li>FontAwesome (icons set);</li>
+        </ul>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
